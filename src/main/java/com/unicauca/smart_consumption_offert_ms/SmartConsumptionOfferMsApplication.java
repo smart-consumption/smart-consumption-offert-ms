55675cc86fe1909fe6b1a708f5db8756b6dbae59
Jdcarsa <jcardenass@unicauca.edu.co>
2024-10-28 18:32:36 -0500

    cambiar url

--- src/main/java/com/unicauca/smart_consumption_offert_ms/SmartConsumptionOfferMsApplication.java
+++ src/main/java/com/unicauca/smart_consumption_offert_ms/SmartConsumptionOfferMsApplication.java
@@ -2,12 +2,14 @@ package com.unicauca.smart_consumption_offert_ms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-public class SmartConsumptionOffertMsApplication {
+@EnableDiscoveryClient
+public class SmartConsumptionOfferMsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SmartConsumptionOffertMsApplication.class, args);
+		SpringApplication.run(SmartConsumptionOfferMsApplication.class, args);
 	}
 
 }
--- src/test/java/com/unicauca/smart_consumption_offert_ms/SmartConsumptionOfferMsApplicationTests.java
+++ src/test/java/com/unicauca/smart_consumption_offert_ms/SmartConsumptionOfferMsApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SmartConsumptionOffertMsApplicationTests {
+class SmartConsumptionOfferMsApplicationTests {
 
 	@Test
 	void contextLoads() {
